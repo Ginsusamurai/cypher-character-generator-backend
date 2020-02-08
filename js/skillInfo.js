053d212af92353e13536de3a5ad17ba472487d61
Morgan <heinemann.morgan@gmail.com>
2020-02-07 17:04:29 -0800

    more data structures

---
+++ js/skillInfo.js
@@ -0,0 +1,6 @@
+'use strict'
+
+var skillList = [
+  {name:'text', cost:'text', type:'action/enabler', description:'text',},
+  {name:'Anticipation', cost:'1 Intellect', type:'Action', description:'You look ahead to see how your actions might unfold. The first task you perform before the end of the next round gains an asset',}
+];
---
+++ js/typeInfo.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var typeInfo = [
+  {name:'Warrior',
+    might: 10,
+    speed: 10,
+    intellect: 8,
+    extraPool: 6,
+    edge: ['1 might','1 speed'],
+    weapons:['light','medium','heavy'],
+    cypherMax: 2,
+    t1:['Bash', 'Combat Prowess', 'Control the Field', 'Improved Edge', 'No Need for Weapons', 'Overwatch', 'Physical Skills', 'Practiced in Armor', 'Quick Throw', 'Swipe', 'Trained Without Armor'],}
+];
