c7aeeaeb915479ad3356a206366f472e2daecd81
Morgan <heinemann.morgan@gmail.com>
2020-02-19 16:49:03 -0800

    heroku db hooked up in server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -8,6 +8,21 @@ app.use(cors());
 const PORT = process.env.PORT || 3001;
 app.use(express.static('.public'));
 
+const pg = require('pg');
+const client = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema, table_name FROM information_schema.tables;', (err,res) => {
+  if (err) throw err;
+  for (let row of res.rows){
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 
 app.get('/', (request, response) => {
