c48a1b5d48477f431ae0532294883c9ff642897c
Morgan <heinemann.morgan@gmail.com>
2020-03-16 14:50:02 -0700

    added more schemas, test of csv injection works, need further scaling out

--- schema.sql
+++ schema.sql
@@ -1,14 +1,53 @@
 DROP TABLE IF EXISTS skillInfo;
+DROP TABLE IF EXISTS typeinfo;
+DROP TABLE IF EXISTS focusskills;
+DROP TABLE IF EXISTS descriptorskills;
+DROP TABLE IF EXISTS descriptorlist;
 
-CREATE TABLE skillInfo (
+CREATE TABLE skillinfo (
   skill_name VARCHAR (255),
   warrior_tier NUMERIC (1),
   adept_tier NUMERIC (1),
   explorer_tier NUMERIC (1),
   speaker_tier NUMERIC (1),
-  general_tier NUMERIC (1),
+  general_tier VARCHAR (255),
   skill_type VARCHAR (255),
   point_cost NUMERIC (2),
   pool_type VARCHAR (255),
   description TEXT
 );
+
+CREATE TABLE typeinfo (
+  type_name VARCHAR (255),
+  might NUMERIC(2),
+  speed NUMERIC(2),
+  intellect NUMERIC(2),
+  extra NUMERIC(2),
+  effort NUMERIC (1),
+  cypher NUMERIC (1),
+  edge NUMERIC (1),
+  edge_limit NUMERIC(1),
+  abilities NUMERIC(1),
+  starting_items TEXT, 
+  weapons VARCHAR (10)
+);
+
+CREATE TABLE focusskills (
+  focus_name VARCHAR(255),
+  tier NUMERIC (1),
+  choice BOOLEAN,
+  focus_skill_name VARCHAR (255)
+);
+
+CREATE TABLE descriptorskills (
+  descriptor_name VARCHAR (255),
+  descriptor_skill_name VARCHAR (255),
+  descriptor_skill_type VARCHAR (255),
+  descriptor_skill_value TEXT
+);
+
+CREATE TABLE descriptorlist (
+  descriptor_name VARCHAR (255),
+  decriptor_description TEXT
+);
+
