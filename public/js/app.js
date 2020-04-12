725ecc5b40f3f0b7a6ba0c2c4b342591330c20a9
Morgan <heinemann.morgan@gmail.com>
2020-04-12 15:26:38 -0700

    query data and prep index.ejs for formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,32 @@
+
+// const descriptorQuery = require('./dataFetchers/descriptorQuery');
+// const typeQuery = require('./dataFetchers/typQuery');
+// const focusQuery = require('./dataFetchers/focusQuery');
+
 function descriptorFetchHandler(e) {
   let descriptorName = e.target.value;
-  console.log(descriptorName);
+  descriptorQuery(descriptorName)
+    .then(data => {
+      console.log('desc!', data);
+    });
 }
 
 function typeFetchHandler(e){
   let typeName = e.target.value;
   console.log(typeName);
+  typeQuery(typeName)
+    .then(data => {
+      console.log('type', data);
+    });
 }
 
 function focusFetchHandler(e){
   let focusName = e.target.value;
   console.log(focusName);
+  focusQuery(focusName)
+    .then(data => {
+      console.log('focus', data);
+    });
 }
 
 // fetch descriptor data
